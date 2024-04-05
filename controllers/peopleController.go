5e69cc54a84492c623c073a5c36cfa89fe8945c4
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2024-04-05 19:23:45 -0400

    pagination html + structure

--- controllers/peopleController.go
+++ controllers/peopleController.go
@@ -2,19 +2,35 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/robbyklein/pages/initializers"
 	"github.com/robbyklein/pages/models"
 )
 
+type PaginationData struct {
+	NextPage     int
+	PreviousPage int
+	CurrentPage  int
+}
+
 func PeopleIndexGET(c *gin.Context) {
+	// GET PAGE number
+	pageStr := c.Param("page")
+	page, _ := strconv.Atoi(pageStr) // dont care about error here
+	offset := (page - 1) * 10
 	// Get the people
 	var people []models.Person
-	initializers.DB.Find(&people)
+	initializers.DB.Limit(10).Offset(offset).Find(&people)
 
 	// Render the page
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"people": people,
+		"pagination": PaginationData{
+			NextPage:     page + 1,
+			PreviousPage: page - 1,
+			CurrentPage:  page,
+		},
 	})
 }
