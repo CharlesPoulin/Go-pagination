44ab6eec6e946884cbdd49b8542621d124f4f8a8
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2024-04-05 19:43:58 -0400

    previous

--- controllers/peopleController.go
+++ controllers/peopleController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/robbyklein/pages/initializers"
 	"github.com/robbyklein/pages/models"
+	"math"
 	"net/http"
 	"strconv"
 )
@@ -12,17 +13,25 @@ type PaginationData struct {
 	NextPage     int
 	PreviousPage int
 	CurrentPage  int
+	TotalPages   int
 }
 
 func PeopleIndexGET(c *gin.Context) {
 	// GET PAGE number
+	perPage := 10
 	page := 1
 	pageStr := c.Param("page")
 
 	if pageStr != "" {
 		page, _ = strconv.Atoi(pageStr) // dont care about error here
 	}
-	offset := (page - 1) * 10
+
+	// calculate total pages for next
+	var totalRows int64
+	initializers.DB.Model(&models.Person{}).Count(&totalRows)
+	totalpages := math.Ceil(float64(totalRows / int64(perPage)))
+
+	offset := (page - 1) * perPage
 	// Get the people
 	var people []models.Person
 	initializers.DB.Limit(10).Offset(offset).Find(&people)
@@ -34,6 +43,7 @@ func PeopleIndexGET(c *gin.Context) {
 			NextPage:     page + 1,
 			PreviousPage: page - 1,
 			CurrentPage:  page,
+			TotalPages:   int(totalpages),
 		},
 	})
 }
